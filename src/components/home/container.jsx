69953f567e706b8845f098d64420a5875fcc8f9d
Léo Baccialone <leo.baccialone@globalservs.com>
2023-02-08 15:10:25 +0100

    deploy 1.03 || change router

--- src/components/home/container.jsx
+++ src/components/home/container.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
-let Home = function() {
+let Home = function () {
 
 
-    return(
+    return (
         <div className="home">
-           
+
             <div className="home__content">
                 <div className="home__textBloc">
                     <h5 className="textBloc__heading5">SO, YOU WANT TO TRAVEL TO SPACE</h5>
@@ -15,7 +15,7 @@ let Home = function() {
                     <p className="textBloc__bodyText bodyText">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                 </div>
                 <div className="home__explore">
-                    <NavLink to="/destinations"><div className="explore__text">EXPLORE</div></NavLink>
+                    <NavLink to="/spaceTourism/destinations"><div className="explore__text">EXPLORE</div></NavLink>
                 </div>
             </div>
         </div>
